4a58805bbccb88da914ce295fa8201a404ce168d
whhjdi <418605872@qq.com>
2019-01-11 17:45:56 +0800

    尝试netlify

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/shop-vue/dist/" : "/"
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/shop-vue/dist/" : "/"
+// };
